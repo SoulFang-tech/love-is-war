c39011a7f55411cf33952e193c97ab9aabc15344
Vedanta SINGH <65032954+SoulFang-tech@users.noreply.github.com>
2021-04-23 21:53:13 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from playsound import playsound
+print('1. Love Dramatic')
+
+playsound('https://soulfang-tech.github.io/love-is-war/Love_Dramatic.mp3')
+
+##########################################################################
+
+print('2. DADDY!_DADDY!_DO!')
+
+playsound('https://soulfang-tech.github.io/love-is-war/DADDY!_DADDY!_DO!.mp3')
+
+###########################################################################
+
+print('3. Kazi no Fukarate')
+
+playsound('https://soulfang-tech.github.io/love-is-war/Kaze_ni%20_Fukarete.mp3')
+
+############################################################################
+
+print('4. Chika Dance')
+print('https://soulfang-tech.github.io/love-is-war/Chika-Dance.mp3')
+
+exit()
