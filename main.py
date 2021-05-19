0211e2c89f633d3c7f8904c80efcd1a4e68d61fb
SolFang-tech <magnetkingata@gmail.com>
2021-05-19 14:56:15 +0530

    updated Main.py to include new songs

--- main.py
+++ main.py
@@ -1,25 +1,50 @@
 from playsound import playsound
-print('1. Love Dramatic')
-print("####################################################################")
-playsound('https://soulfang-tech.github.io/love-is-war/Love_Dramatic.mp3')
 
-##########################################################################
 
-print('2. DADDY!_DADDY!_DO!')
-print("###################################################################")
+while True:
+        print('1. Love Dramatic')
 
-playsound('https://soulfang-tech.github.io/love-is-war/DADDY!_DADDY!_DO!.mp3')
+        print("####################################################################")
+        playsound('https://soulfang-tech.github.io/music/Love_Dramatic.mp3')
 
-###########################################################################
+        ##########################################################################
 
-print('3. Kazi no Fukarate')
-print("####################################################################")
+        print('2. DADDY!_DADDY!_DO!')
 
-playsound('https://soulfang-tech.github.io/love-is-war/Kaze_ni%20_Fukarete.mp3')
+        print("###################################################################")
 
-############################################################################
+        playsound('https://soulfang-tech.github.io/love-is-war/DADDY!_DADDY!_DO!.mp3')
 
-print('4. Chika Dance')
-playsound('https://soulfang-tech.github.io/love-is-war/Chika-Dance.mp3')
-print("______________________________________________________________________")
-exit()
+        ############################################################################
+
+        print('3. Chika Dance')
+
+        playsound('https://soulfang-tech.github.io/love-is-war/Chika-Dance.mp3')
+
+        print("_____________________________over the moon for you_________________________________________")
+
+        print("1. Koi no Uta")
+
+        playsound('https://soulfang-tech.github.io/music/Koi_no_Uta.mp3')
+
+        print("####################################################################")
+
+        ############################################################################
+
+        print("2. Tsuki to Hoshizora ")
+
+        playsound('https://soulfang-tech.github.io/music/Tonikaku_Kawaii_Ending.mp3')
+
+        print("####################################################################")
+
+        ############################################################################
+
+        print("3. Koi no Uta Dubbed")
+
+        playsound('https://soulfang-tech.github.io/music/Tonikawa_%20Over_The_Moon_For_You_.mp3')
+
+        print("####################################################################")
+
+        ############################################################################
+
+        exit()
